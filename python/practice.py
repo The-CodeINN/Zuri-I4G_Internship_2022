9490917d6a70a158edd34842019e535cd6871c62
Fairbuyng <emijere.richard@gmail.com>
2022-05-26 20:08:30 +0100

    Learnt new things

--- python/practice.py
+++ python/practice.py
@@ -5,11 +5,9 @@ name = "richard "
 
 # print(f"My name is {name.title().rstrip()}")
 
-places_in_the_world = ['nigeria', 'ghana',
-                       'cameroun', 'djibouti', 'madagascar']
 
-for places in sorted(places_in_the_world[:-3]):
-    print(places)
+# for places in sorted(places_in_the_world[:-3]):
+#     print(places)
 
 
 number = list(range(1, 4))
@@ -23,3 +21,14 @@ for i in range(1, 11):
 
 cubes = [value ** 3 for value in range(1, 11)]
 # print(cubes)
+
+nums = (1,2,3,4)
+
+places_in_the_world = ['nigeria', 'ghana',
+                       'cameroun', 'djibouti', 'madagascar']
+
+for places in places_in_the_world:
+    if places == "nigeria":
+        print(places.title())
+    else:
+      print(places.upper())
